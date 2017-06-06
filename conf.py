be5f7ff14483c2feae477289ca899763008b48f9
Tony Narlock <tony@git-pull.com>
2017-06-06 13:19:58 -0500

    add flask-sqlalchemy

--- conf.py
+++ conf.py
@@ -81,6 +81,7 @@ intersphinx_mapping = {
         'http://docs.djangoproject.com/en/1.11/_objects/'
     ),
     'flask': ('http://flask.pocoo.org/docs/', None),
+    'flask-sqlalchemy': ('http://flask-sqlalchemy.pocoo.org/2.2/', None),
     'werkzeug': ('http://werkzeug.pocoo.org/docs/', None),
     'jinja': ('http://jinja2.pocoo.org/docs/dev', None),
     'sqlalchemy': ('http://docs.sqlalchemy.org/en/latest/', None)
