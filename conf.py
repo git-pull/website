bc8b945f22cde49ae4ee0a5dff8272bf4a4e079f
Tony Narlock <tony@git-pull.com>
2017-08-20 11:08:44 -0500

    update intersphinx mappings to https

--- conf.py
+++ conf.py
@@ -64,8 +64,8 @@ texinfo_documents = [
 ]
 
 intersphinx_mapping = {
-    'http://docs.python.org/': None,
-    'pip': ('http://sphinx.readthedocs.org/en/latest/', None),
+    'https://docs.python.org/2/': None,
+    'sphinx': ('https://sphinx.readthedocs.io/en/latest/', None),
     'tmuxp': ('https://tmuxp.git-pull.com/en/latest/', None),
     'libtmux': ('https://libtmux.git-pull.com/en/latest/', None),
     'libvcs': ('https://libvcs.git-pull.com/en/latest/', None),
@@ -73,17 +73,14 @@ intersphinx_mapping = {
     'cihai': ('https://cihai.git-pull.com/en/latest/', None),
     'cihai-cli': ('https://cihai-cli.git-pull.com/en/latest/', None),
     'unihan-etl': ('https://unihan-etl.git-pull.com/en/latest/', None),
-    'dockerjournal': (
-      'http://docker-recipes.readthedocs.org/en/latest/', None
-    ),
     'django': (
-        'http://docs.djangoproject.com/en/1.11/',
-        'http://docs.djangoproject.com/en/1.11/_objects/'
+        'https://docs.djangoproject.com/en/1.11/',
+        'https://docs.djangoproject.com/en/1.11/_objects/'
     ),
     'flask': ('http://flask.pocoo.org/docs/', None),
     'flask-sqlalchemy': ('http://flask-sqlalchemy.pocoo.org/2.2/', None),
     'werkzeug': ('http://werkzeug.pocoo.org/docs/0.12/', None),
-    'jinja': ('http://jinja2.pocoo.org/docs/dev', None),
+    'jinja': ('http://jinja.pocoo.org/docs/dev', None),
     'sqlalchemy': ('http://docs.sqlalchemy.org/en/latest/', None),
     'uwsgi': ('https://uwsgi-docs.readthedocs.io/en/latest/', None),
 }
